e7046c7f6260129549675f867fc69dd926ad5041
Maxi Gorynski <supriyarai@Supriyas-MacBook-Pro.local>
2024-11-13 15:35:33 +0000

    Added extensions file, routes debugging

--- auth/routes.py
+++ auth/routes.py
@@ -1,4 +1,4 @@
-from everstone import bcrypt
+from everstone.extensions import bcrypt
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import logout_user, login_required, login_user
 from .forms import RegistrationForm, LoginForm
